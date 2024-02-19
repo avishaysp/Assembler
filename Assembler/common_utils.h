1accb4045d0496833d0a1ab3eba79d479510a7fe
Avishay Spitzer <a_spitzer@apple.com>
2024-02-19 18:57:13 +0200

    common utils

---
+++ Assembler/common_utils.h
@@ -0,0 +1,22 @@
+#pragma once
+#include <stdio.h>
+
+
+enum op_code {
+    add,
+    sub,
+    mac,
+    and,
+    or,
+    xor,
+    sll,
+    sra,
+    srl,
+    beq,
+    bne,
+    blt,
+    bgt,
+    ble,
+    bge,
+    jal
+};
